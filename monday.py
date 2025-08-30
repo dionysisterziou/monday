76dc3a5178eb6181c98be53fe93d60bf02560c1a
dionysisterziou <dionysisterziou@gmail.com>
2025-08-30 22:13:34 +0300

    Add some more functions

--- monday.py
+++ monday.py
@@ -1,6 +1,12 @@
 print('Maria: Welcome to our family! What is your name?')
+
 my_name = input()
+
 print(my_name + ': Hi, my name is ' + my_name + '! What about yours?')
 print('Maria: Nice to meet you ' + my_name + '. I am Maria! I will order coffee, what coffee would you like?')
-my_coffee = input()
-print(my_name + ': Sure! I want a ' + my_coffee + '!')
+print(my_name + ': I want a cappucccino assassino!')
+print('Maria: Pefect! Also, we do a test to newcommers! First, tell me the length of your name!')
+
+length_of_name = str(len(my_name)) # change it later to my_name
+
+print('Dionysis: It\'s ' + length_of_name + '!')
