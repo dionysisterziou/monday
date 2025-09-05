5c2e1aa6283829c39d0ef4b21a07f59aa0afcc3a
dionysisterziou <dionysisterziou@gmail.com>
2025-09-05 22:19:30 +0300

    Add while loops and other

--- monday.py
+++ monday.py
@@ -2,11 +2,26 @@ print('Maria: Welcome to our family! What is your name?')
 
 my_name = input()
 
+while my_name == '':
+    print('Maria: Oh... so you don\'t have name? Try again!')
+    my_name = input()
+
 print(my_name + ': Hi, my name is ' + my_name + '! What about yours?')
 print('Maria: Nice to meet you ' + my_name + '. I am Maria! I will order coffee, what coffee would you like?')
-print(my_name + ': I want a cappucccino assassino!')
-print('Maria: Pefect! Also, we do a test to newcommers! First, tell me the length of your name!')
+print(my_name + ': I want a cappuccino assassino!')
+print('Maria: ' + 'HA' * 3 + '! Anyway... we do a test to newcommers! First, tell me the length of your name!')
 
-length_of_name = str(len(my_name)) # change it later to my_name
+length_of_name = len(my_name)
 
-print('Dionysis: It\'s ' + length_of_name + '!')
+print(my_name +': It\'s ' + str(length_of_name) + '!')
+
+if length_of_name < 10:
+    print('Maria: Oh, you\'re a Jedi!')
+
+    force_counter = 0
+
+    while counter < 3:
+        print(my_name + ': May the force be with you!')
+        force_counter = force_counter + 1
+else:
+    print('Maria: Oh, you\'re a Sith!')
