13c1e56e996fad65907fb18a619009b4d8dda139
dionysisterziou <dionysisterziou@gmail.com>
2025-09-07 18:16:37 +0300

    Make dialogues with Maria more interactive

--- monday.py
+++ monday.py
@@ -2,26 +2,33 @@ print('Maria: Welcome to our family! What is your name?')
 
 my_name = input()
 
-while my_name == '':
-    print('Maria: Oh... so you don\'t have name? Try again!')
+while not my_name:
+    print('Maria: Oh... so you don\'t have a name? Try again!')
     my_name = input()
 
 print(my_name + ': Hi, my name is ' + my_name + '! What about yours?')
 print('Maria: Nice to meet you ' + my_name + '. I am Maria! I will order coffee, what coffee would you like?')
 print(my_name + ': I want a cappuccino assassino!')
-print('Maria: ' + 'HA' * 3 + '! Anyway... we do a test to newcommers! First, tell me the length of your name!')
+print('Maria: ' + 'HA' * 3 + '! Anyway... we do a test to newcommers! Tell me the length of your name!')
 
 length_of_name = len(my_name)
 
 print(my_name +': It\'s ' + str(length_of_name) + '!')
+line = ''
 
 if length_of_name < 10:
-    print('Maria: Oh, you\'re a Jedi!')
+    line = 'May the force be with you'
+    print('Maria: Oh, you\'re a Jedi! This means that you have to say ' + str(length_of_name) + ' times "' + line + '"!')
 
-    force_counter = 0
-
-    while counter < 3:
-        print(my_name + ': May the force be with you!')
-        force_counter = force_counter + 1
+    for _ in range(length_of_name):
+        print(my_name + ': ' + line + '!')
 else:
-    print('Maria: Oh, you\'re a Sith!')
+    line = 'Infinite POWER'
+    print('Maria: Oh, you\'re a Sith! This means that you have to say ' + str(length_of_name) + ' times "' + line + '"!')
+
+    for _ in range(length_of_name):
+        print(my_name + ': ' + line + '!')
+
+print('Maria: Yey! Oh... the coffee arrived! Let me count my money...')
+for i in range(2, 10, 2):
+    print('Maria: ' + str(i) + '.')
